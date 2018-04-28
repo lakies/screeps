bb888389b214589723b02eb25f2cff861b2711a0
lakies <kirikaladrian@gmail.com>
2018-04-28 19:33:31 +0300

    finished generic miner

--- creepController.js
+++ creepController.js
@@ -1,14 +1,5 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('creepController');
- * mod.thing == 'a thing'; // true
- */
-
 module.exports = {
-    run: function(creep){
+    run: function(creep){//TODO: add check for time to live and do something about it
         if(creep.memory.job !== undefined){
             switch(creep.memory.job.type){
                 case 'genericMining':
@@ -31,33 +22,47 @@ module.exports = {
 
     },
 
-    genericMiner: function (creep) {
+    genericMiner: function (creep) {//TODO: maybe replace spots with flags
         var job = creep.memory.job;
-        if(!creep.memory.target){
-            creep.memory.target = job.target_id;
-        }
-        if(!creep.memory.targetSpot){
-            creep.memory.targetSpot = new RoomPosition(job.spot.x,job.spot.y,job.spot.room);
-        }
 
         if(creep.pos.isEqualTo(new RoomPosition(job.spot.x,job.spot.y,job.spot.room))){//maybe this new object generation is too costly each time, make an internal var if arrived?
-            var result = creep.harvest(Game.getObjectById(creep.memory.target));
+            var result = creep.harvest(Game.getObjectById(job.target_id));
             if(result !== 0 && result !== -4){
                 console.log('Failed to harvest: error ' + result);
             }
         }else{
+            console.log(creep.moveTo(new RoomPosition(job.spot.x,job.spot.y,job.spot.room)));//don't need to console log every time probs
+        }
 
-            console.log(creep.moveTo(new RoomPosition(job.spot.x,job.spot.y,job.spot.room)));
+        if(creep.carry.energy === creep.carryCapacity){
+            creep.memory.lastSpot = job.spot;
+            for(var i in Memory.spawns[creep.memory.spawn].sources){
+                for(var u in Memory.spawns[creep.memory.spawn].sources[i].freeSpots){
+                    var spot1 =  Memory.spawns[creep.memory.spawn].sources[i].freeSpots[u];
+                    if(JSON.stringify(job.spot) === JSON.stringify(spot1)){
+                        spot1.assigned = false;
+                        creep.memory.lastSpot.assigned = false;
+                        break;
+                    }
+                }
+            }
+            creep.memory.job = {type: 'hauler'};
         }
     },
 
     hauler: function (creep) {
-
+        //TODO: first check for empty structures, then if full upgrade controller
+        //TODO: note down somewhere how many energy is delivered to a structure to avoid overfilling
     },
 
     mover: function (creep) {
 
+    },
+
+    upgrader: function (creep){
+
     }
+
 
 
 };
